2359f8df725e6dff15735c15d584d593c5f2df21
NTMNathan <34463340+NTMNathan@users.noreply.github.com>
2021-10-15 12:16:21 +0000

    Deploying to gh-pages from @ ntm-development/weeby-js-site@d085b9670c1af85ef5276049b12ab7e90ce1ebda 🚀

---
+++ assets/Spinner.9cf914c7.js
@@ -0,0 +1,1 @@
+import{o as s,b as a,e}from"./vendor.79db57bd.js";const o={},r={class:"relative h-10 w-10 my-60 mx-auto"},d=e("div",{class:"cube1 bg-discord-700 h-4 w-4 absolute"},null,-1),l=e("div",{class:"cube2 bg-discord-700 h-4 w-4 absolute"},null,-1);o.render=function(e,o){return s(),a("div",r,[d,l])};var b=o;export{b as S};
